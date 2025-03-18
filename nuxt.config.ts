f54c50d8609c948a8a7ebc422359c3e94c501620
Yazid Zaidan Mujadid <yazidzm.developer@gmail.com>
2025-03-18 13:32:27 +0700

    build: add nuxt content and tailwind config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/content', '@nuxtjs/tailwindcss', '@pinia/nuxt'],
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image'],
   srcDir: './src',
+  compatibilityDate: '2024-11-01',
   telemetry: {
     enabled: false,
-  }
+  },
 })
---
+++ tailwind.config.ts
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/components/**/*.{vue,js,ts,tsx}',
+    './src/layouts/**/*.{vue,js,ts,tsx}',
+    './src/pages/**/*.{vue,js,ts,tsx}',
+  ],
+}
